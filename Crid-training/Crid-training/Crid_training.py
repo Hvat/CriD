aee8f9e05aa5c00d93f3ac62beda5cf1988679b8
Artem Evdokimov <obhvat@inbox.ru>
2020-03-09 15:00:27 +0500

    Global variables declared

--- Crid-training/Crid-training/Crid_training.py
+++ Crid-training/Crid-training/Crid_training.py
@@ -12,3 +12,11 @@ import cntk.tests.test_utils
 cntk.tests.test_utils.set_device_from_pytest_env()
 np.random.seed(0)
 
+# Global variables
+EXPORT_BARS = 6720
+INPUT_DIM = 72
+OUTPUT_DIM = 1
+SIZE = INPUT_DIM + OUTPUT_DIM
+EPOCHS = 1000
+LOSS = 0.00038
+LEARNING_RATE = 0.1
